121a41fcd71131993f793520f8c778d9d1dc8c5b
高赞 <gaozan@anchnet.com>
2019-07-06 18:35:05 +0800

    商品分类列表

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -30,6 +30,10 @@ class _HomePageState extends State<HomePage>
   int _currentIndex = 0;
   List _hotWords = [];
 
+  bool _onScroll(ScrollNotification scroll) {
+    return false;
+  }
+
   String get hoursString {
     Duration duration =
         _animationController.duration * _animationController.value;
@@ -152,16 +156,7 @@ class _HomePageState extends State<HomePage>
             )
           : TabBarView(
               controller: _controller,
-              children: <Widget>[
-                Text('全部'),
-                Text('直播'),
-                Text('便宜好货'),
-                Text('买家秀'),
-                Text('全球'),
-                Text('生活'),
-                Text('母婴'),
-                Text('时尚'),
-              ],
+              children: _createGoodsList(),
             ),
     );
     return Scaffold(
@@ -188,6 +183,77 @@ class _HomePageState extends State<HomePage>
         ],
       ),
     );
+  }
+
+  // 分类商品
+  List<Widget> _createGoodsList() {
+    return [8, 7, 6, 5, 4, 3, 2, 1].map((item) {
+      return SingleChildScrollView(
+        padding: EdgeInsets.all(
+          ScreenUtil.getInstance().setWidth(15),
+        ),
+        child: Wrap(
+          spacing: ScreenUtil.getInstance().setWidth(12),
+          runSpacing: ScreenUtil.getInstance().setHeight(20),
+          children: [1, 2, 3, 4, 5, 6, 7, 8].map((item1) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                width: ScreenUtil.getInstance().setWidth(354),
+                height: ScreenUtil.getInstance().setHeight(528),
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: ScreenUtil.getInstance().setWidth(354),
+                      height: ScreenUtil.getInstance().setHeight(262),
+                      child: GZCacheNetworkImageWidget(
+                        imageUrl:
+                            'https://img.alicdn.com/tfscom/i1/2610856926/TB2JAdPhwRkpuFjy1zeXXc.6FXa_!!2610856926.jpg_360x360xzq90.jpg_.webp',
+                      ),
+                    ),
+                    SizedBox(
+                      height: ScreenUtil.getInstance().setHeight(10),
+                    ),
+                    Text(
+                      '红米5 Plus',
+                      style: TextStyle(
+                        fontSize: ScreenUtil.getInstance().setSp(32),
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(80, 80, 80, 1),
+                      ),
+                    ),
+                    Text(
+                      '千元全面屏，前置柔光自拍',
+                      style: TextStyle(
+                        fontSize: ScreenUtil.getInstance().setSp(24),
+                        color: Color.fromRGBO(80, 80, 80, 1),
+                      ),
+                    ),
+                    Text(
+                      '999起',
+                      style: TextStyle(
+                        fontSize: ScreenUtil.getInstance().setSp(32),
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(134, 134, 134, 1),
+                      ),
+                    ),
+                    MaterialButton(
+                      height: ScreenUtil.getInstance().setHeight(58),
+                      minWidth: ScreenUtil.getInstance().setWidth(192),
+                      color: Colors.red,
+                      textColor: Colors.white,
+                      child: new Text('立即购买'),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      );
+    }).toList();
   }
 
   // 轮播图
