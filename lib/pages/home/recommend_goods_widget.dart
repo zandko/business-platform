a112ca5b0058cec9603e7a709aff9b37cbc2389b
高赞 <gaozan@anchnet.com>
2019-07-16 10:54:13 +0800

    商品详情页面

--- lib/pages/home/recommend_goods_widget.dart
+++ lib/pages/home/recommend_goods_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shop_app/components/cache_network_image.dart';
+import 'package:shop_app/pages/product_details/product_details_page.dart';
 
 class RecommendGoodsWidget extends StatelessWidget {
   @override
@@ -11,30 +12,34 @@ class RecommendGoodsWidget extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          GZCacheNetworkImageWidget(
-            width: ScreenUtil.getInstance().setWidth(373),
-            height: ScreenUtil.getInstance().setHeight(534),
-            imageUrl: 'https://i1.mifile.cn/a4/xmad_15592788710155_nYAat.jpg',
+          GestureDetector(
+            child: _buildRecommendGoods(373, 534, 'https://i1.mifile.cn/a4/xmad_15592788710155_nYAat.jpg'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProductDetailsPage(),
+                ),
+              );
+            },
           ),
           Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              GZCacheNetworkImageWidget(
-                width: ScreenUtil.getInstance().setWidth(367),
-                height: ScreenUtil.getInstance().setHeight(258),
-                imageUrl:
-                'https://i1.mifile.cn/a4/xmad_15621496532895_qtPax.jpg',
-              ),
-              GZCacheNetworkImageWidget(
-                width: ScreenUtil.getInstance().setWidth(367),
-                height: ScreenUtil.getInstance().setHeight(264),
-                imageUrl:
-                'https://i1.mifile.cn/a4/xmad_15622932130351_aJFEP.jpg',
-              ),
+              _buildRecommendGoods(367, 258, 'https://i1.mifile.cn/a4/xmad_15621496532895_qtPax.jpg'),
+              _buildRecommendGoods(367, 264, 'https://i1.mifile.cn/a4/xmad_15622932130351_aJFEP.jpg'),
             ],
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildRecommendGoods(double width, double height, String imageUrl) {
+    return GZCacheNetworkImageWidget(
+      width: ScreenUtil.getInstance().setWidth(width),
+      height: ScreenUtil.getInstance().setHeight(height),
+      imageUrl: imageUrl,
     );
   }
 }
---
+++ lib/pages/product_details/product_details_page.dart
@@ -0,0 +1,519 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:shop_app/data/home.dart';
+import 'package:shop_app/components/cache_network_image.dart';
+
+class ProductDetailsPage extends StatefulWidget {
+  @override
+  _ProductDetailsPageState createState() => _ProductDetailsPageState();
+}
+
+class _ProductDetailsPageState extends State<ProductDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              bottom: ScreenUtil.getInstance().setHeight(104),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  _buildGoodsAlbumWidget(),
+                  _buildDescriptionWidget(),
+                  _buildDeliveryWidget(),
+                  _buildAccessoriesWidget(),
+                  _buildEvaluationWidget(),
+                  _buildParameterWidget(),
+                ],
+              ),
+            ),
+          ),
+          _buildActionBarWidget(),
+          _buildBottomBarWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionBarWidget() {
+    return Positioned(
+      top: ScreenUtil.getInstance().setHeight(66),
+      child: Container(
+        padding: EdgeInsets.only(
+          left: ScreenUtil.getInstance().setWidth(20),
+          right: ScreenUtil.getInstance().setWidth(20),
+        ),
+        width: MediaQuery.of(context).size.width,
+        height: ScreenUtil.getInstance().setHeight(86),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            ClipOval(
+              child: InkWell(
+                child: Container(
+                  width: ScreenUtil.getInstance().setWidth(66),
+                  height: ScreenUtil.getInstance().setHeight(66),
+                  color: Color.fromRGBO(56, 56, 56, 0.6),
+                  child: Icon(Icons.chevron_left, color: Colors.white),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            ClipOval(
+              child: Container(
+                width: ScreenUtil.getInstance().setWidth(66),
+                height: ScreenUtil.getInstance().setHeight(66),
+                color: Color.fromRGBO(56, 56, 56, 0.6),
+                child: Icon(Icons.more_horiz, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBottomBarWidget() {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      child: Container(
+        color: Colors.white,
+        height: ScreenUtil.getInstance().setHeight(104),
+        child: Row(
+          children: <Widget>[
+            Container(
+              width: ScreenUtil.getInstance().setWidth(328),
+              height: ScreenUtil.getInstance().setHeight(102),
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(width: 1, color: Colors.black12),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.favorite_border,
+                        color: Color.fromRGBO(128, 128, 128, 1),
+                        size: ScreenUtil.getInstance().setSp(42),
+                      ),
+                      Text(
+                        '喜欢',
+                        style: TextStyle(
+                          color: Color.fromRGBO(128, 128, 128, 1),
+                          fontSize: ScreenUtil.getInstance().setSp(24),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: ScreenUtil.getInstance().setHeight(50),
+                    width: 1,
+                    color: Color.fromRGBO(128, 128, 128, 1),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.shopping_cart,
+                        color: Color.fromRGBO(128, 128, 128, 1),
+                        size: ScreenUtil.getInstance().setSp(42),
+                      ),
+                      Text(
+                        '购物车',
+                        style: TextStyle(
+                          color: Color.fromRGBO(128, 128, 128, 1),
+                          fontSize: ScreenUtil.getInstance().setSp(24),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: ScreenUtil.getInstance().setWidth(422),
+              height: ScreenUtil.getInstance().setHeight(104),
+              color: Color.fromRGBO(255, 112, 58, 1),
+              child: Text(
+                '加入购物车',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: ScreenUtil.getInstance().setSp(30),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGoodsAlbumWidget() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: ScreenUtil.getInstance().setHeight(824),
+      child: Swiper(
+        index: 0,
+        loop: true,
+        autoplay: true,
+        duration: 300,
+        pagination: SwiperPagination(),
+        scrollDirection: Axis.horizontal,
+        itemCount: banner_images.length,
+        onTap: (index) {
+          print(index);
+        },
+        itemBuilder: (context, index) {
+          return GZCacheNetworkImageWidget(
+            height: ScreenUtil.getInstance().setHeight(824),
+            imageUrl: banner_images[index],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildDescriptionWidget() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.only(
+        top: ScreenUtil.getInstance().setHeight(28),
+        left: ScreenUtil.getInstance().setWidth(28),
+        right: ScreenUtil.getInstance().setWidth(24),
+      ),
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            '米家扫地机器人',
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            style: TextStyle(
+              color: Color.fromRGBO(51, 51, 51, 1),
+              fontSize: ScreenUtil.getInstance().setSp(36),
+            ),
+          ),
+          Text(
+            '【小米2017感恩季来啦！米家扫地机器人立减100元，用券再享折上优惠，小米分期12期免息】高精度激光测距，智能规划路径/1800Pa 大风压澎湃吸力/远程智能控制，定时清扫/大电池持久清扫',
+            overflow: TextOverflow.ellipsis,
+            maxLines: 4,
+            style: TextStyle(
+              color: Color.fromRGBO(128, 128, 128, 1),
+              fontSize: ScreenUtil.getInstance().setSp(26),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                '￥1599',
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 112, 58, 1),
+                  fontSize: ScreenUtil.getInstance().setSp(36),
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Text(
+                '￥1699',
+                style: TextStyle(
+                  decorationStyle: TextDecorationStyle.solid,
+                  decorationColor: Color.fromRGBO(166, 166, 166, 1),
+                  decoration: TextDecoration.lineThrough,
+                  color: Color.fromRGBO(166, 166, 166, 1),
+                  fontSize: ScreenUtil.getInstance().setSp(26),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 3,
+          ),
+          Container(
+            height: ScreenUtil.getInstance().setHeight(70),
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  width: 1,
+                  color: Color.fromRGBO(229, 229, 229, 1),
+                ),
+              ),
+            ),
+            child: _buildPublicSelectWidget('分期', '花呗分期/小米分期', ''),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPublicSelectWidget(String text1, String text2, String text3) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Text(
+              text1,
+              style: TextStyle(
+                fontSize: ScreenUtil.getInstance().setSp(26),
+                color: Color.fromRGBO(128, 128, 128, 1),
+              ),
+            ),
+            SizedBox(
+              width: ScreenUtil.getInstance().setWidth(40),
+            ),
+            Text(
+              text2,
+              style: TextStyle(
+                fontSize: ScreenUtil.getInstance().setSp(26),
+                color: Color.fromRGBO(51, 51, 51, 1),
+              ),
+            ),
+            SizedBox(
+              width: ScreenUtil.getInstance().setWidth(20),
+            ),
+            Text(
+              text3,
+              style: TextStyle(
+                fontSize: ScreenUtil.getInstance().setSp(26),
+                color: Color.fromRGBO(255, 112, 58, 1),
+              ),
+            ),
+          ],
+        ),
+        Icon(
+          Icons.chevron_right,
+          color: Color.fromRGBO(153, 153, 153, 1),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDeliveryWidget() {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.only(
+        top: ScreenUtil.getInstance().setHeight(24),
+      ),
+      padding: EdgeInsets.only(
+        left: ScreenUtil.getInstance().setWidth(28),
+        right: ScreenUtil.getInstance().setWidth(24),
+      ),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: ScreenUtil.getInstance().setHeight(70),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  width: 1,
+                  color: Color.fromRGBO(229, 229, 229, 1),
+                ),
+              ),
+            ),
+            child: _buildPublicSelectWidget('已选', '米家扫地机器人 x 1', ''),
+          ),
+          Container(
+            height: ScreenUtil.getInstance().setHeight(70),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  width: 1,
+                  color: Color.fromRGBO(229, 229, 229, 1),
+                ),
+              ),
+            ),
+            child: _buildPublicSelectWidget('送至', '北京市 朝阳区', '有现货'),
+          ),
+          Container(
+            height: ScreenUtil.getInstance().setHeight(70),
+            child: Row(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.check_circle_outline,
+                      color: Color.fromRGBO(128, 128, 128, 1),
+                      size: ScreenUtil.getInstance().setSp(28),
+                    ),
+                    Text(
+                      '支持7天无理由退货',
+                      style: TextStyle(
+                        color: Color.fromRGBO(128, 128, 128, 1),
+                        fontSize: ScreenUtil.getInstance().setSp(24),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAccessoriesWidget() {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.only(
+        top: ScreenUtil.getInstance().setHeight(18),
+      ),
+      padding: EdgeInsets.only(
+        left: ScreenUtil.getInstance().setWidth(28),
+        right: ScreenUtil.getInstance().setWidth(24),
+      ),
+      height: ScreenUtil.getInstance().setHeight(70),
+      child: _buildPublicSelectWidget('配件', '虚拟墙、滤网、边刷', ''),
+    );
+  }
+
+  Widget _buildEvaluationWidget() {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+            top: ScreenUtil.getInstance().setHeight(18),
+          ),
+          padding: EdgeInsets.only(
+            left: ScreenUtil.getInstance().setWidth(28),
+            right: ScreenUtil.getInstance().setWidth(24),
+          ),
+          height: ScreenUtil.getInstance().setHeight(70),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              bottom: BorderSide(
+                width: 1,
+                color: Colors.black12,
+              ),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                '用户评价(14257)',
+                style: TextStyle(
+                  fontSize: ScreenUtil.getInstance().setSp(28),
+                  color: Color.fromRGBO(128, 128, 128, 1),
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                        text: '98.6%',
+                        style: TextStyle(
+                          fontSize: ScreenUtil.getInstance().setSp(28),
+                          color: Color.fromRGBO(255, 112, 58, 1),
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '满意',
+                            style: TextStyle(
+                              fontSize: ScreenUtil.getInstance().setSp(28),
+                              color: Color.fromRGBO(128, 128, 128, 1),
+                            ),
+                          ),
+                        ]),
+                  ),
+                  Icon(
+                    Icons.chevron_right,
+                    color: Color.fromRGBO(153, 153, 153, 1),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(
+            ScreenUtil.getInstance().setWidth(26),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  ClipOval(
+                    child: GZCacheNetworkImageWidget(
+                      width: ScreenUtil.getInstance().setWidth(56),
+                      height: ScreenUtil.getInstance().setHeight(56),
+                      imageUrl:
+                          'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3024387196,1621670548&fm=27&gp=0.jpg',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '骑毛驴玩小米',
+                    style: TextStyle(
+                      fontSize: ScreenUtil.getInstance().setSp(30),
+                      color: Color.fromRGBO(56, 56, 56, 1),
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                '小东西很好，双12买的13号就到了，买的时候没告诉老婆，收到之后立马让它先表演一下，结果老婆高兴坏了，出乎我意料的准许我来撩客服妹子，今天我头发理了，胡子刮了，放在衣柜里10多年的西装皮鞋也穿上了',
+                maxLines: 4,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: ScreenUtil.getInstance().setSp(26),
+                  color: Color.fromRGBO(56, 56, 56, 1),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildParameterWidget() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: ScreenUtil.getInstance().setHeight(18),
+      ),
+      height: ScreenUtil.getInstance().setHeight(74),
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text(
+            '商品详情',
+            style: TextStyle(
+              color: Color.fromRGBO(255, 112, 58, 1),
+              fontSize: ScreenUtil.getInstance().setSp(28),
+            ),
+          ),
+          Container(
+            height: ScreenUtil.getInstance().setHeight(40),
+            width: 1,
+            color: Colors.black12,
+          ),
+          Text('参数', style: TextStyle(
+            color: Color.fromRGBO(80, 80, 80, 1),
+            fontSize: ScreenUtil.getInstance().setSp(28),
+          ),),
+        ],
+      ),
+    );
+  }
+}
